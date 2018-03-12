1eda816bde617deadf3e7a55cccdf87036adc46a
金辰浩 <jinchenhao@meituan.com>
2018-03-12 22:26:39 +0800

    ready to release for the first time

--- server/app.js
+++ server/app.js
@@ -16,14 +16,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
-let currentUser = ''; // 
 app.use('/', async function(req, res, next) {
   const user_id = req.cookies.user_id;
   if (user_id) {
